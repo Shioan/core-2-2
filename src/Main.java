03c15ff555e238b9260d01b6bb21691f045de79e
Vladislav Artemov <v.artemov28@yandex.ru>
2023-05-11 19:39:54 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10000000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        long task1 = persons.stream().filter(person -> person.getAge() < 18).count();
+        System.out.println("Кол-во несовершеннолетних " + task1);
+
+        List<String> task2 = persons.stream()
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        //System.out.println("Лица призывного возраста:\n" + task2);
+
+        List<Person> task3 = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 65)
+                .filter(person -> person.getSex() == Sex.WOMAN && person.getAge() >= 18 && person.getAge() <= 60)
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println("Работоспособных с вышкой: \n" + task3);
+    }
+}
